5954bd3248600e3b078b043ff73f9362be11de82
Erik Fisher <ebenfishy@gmail.com>
2018-11-26 09:24:19 -0800

    Start project
    Start project

---
+++ RecordBook/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  RecordBook
+//
+//  Created by Erik Fisher on 11/26/18.
+//  Copyright © 2018 Berk. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
